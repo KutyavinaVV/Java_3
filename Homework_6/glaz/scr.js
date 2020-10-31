b24862f539f0f0f877446244bdb8e73545a78275
KutyavinaVV <65681150+KutyavinaVV@users.noreply.github.com>
2020-11-01 00:14:32 +0400

    Delete scr.js

--- Homework_6/glaz/scr.js
+++
@@ -1,9 +0,0 @@
-let balls = document.getElementsByClassName('eye_black');
-document.onmousemove = function (event) {
-    let x = event.x *75;
-    let y = event.y *75;
-    for (let i = 0; i<2; i++) {
-        balls[i].style.x = x;
-        balls[i].style.y = y;
-    }
-}
